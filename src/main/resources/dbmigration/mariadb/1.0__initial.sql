077cdc23cb303066b14ca920415164a1751fa5a0
Silthus <michael@reichenbach.in>
2020-12-06 23:07:44 +0100

    chore: add rcs_skills migrations

--- src/main/resources/dbmigration/mariadb/1.0__initial.sql
+++ src/main/resources/dbmigration/mariadb/1.0__initial.sql
@@ -1,10 +1,20 @@
 -- apply changes
+create table rcs_skills (
+  id                            varchar(40) not null,
+  alias                         varchar(255),
+  name                          varchar(255),
+  type                          varchar(255),
+  description                   varchar(255),
+  version                       bigint not null,
+  when_created                  datetime(6) not null,
+  when_modified                 datetime(6) not null,
+  constraint pk_rcs_skills primary key (id)
+);
+
 create table rcs_player_skills (
   id                            varchar(40) not null,
-  player_id                     varchar(40),
-  identifier                    varchar(255),
-  name                          varchar(255),
-  description                   varchar(255),
+  player_id                     varchar(40) not null,
+  skill_id                      varchar(40) not null,
   unlocked                      datetime(6),
   active                        tinyint(1) default 0 not null,
   version                       bigint not null,
@@ -25,6 +35,11 @@ create table rcs_players (
   constraint pk_rcs_players primary key (id)
 );
 
+create index ix_rcs_skills_alias on rcs_skills (alias);
+create index ix_rcs_skills_name on rcs_skills (name);
 create index ix_rcs_player_skills_player_id on rcs_player_skills (player_id);
 alter table rcs_player_skills add constraint fk_rcs_player_skills_player_id foreign key (player_id) references rcs_players (id) on delete restrict on update restrict;
+
+create index ix_rcs_player_skills_skill_id on rcs_player_skills (skill_id);
+alter table rcs_player_skills add constraint fk_rcs_player_skills_skill_id foreign key (skill_id) references rcs_skills (id) on delete restrict on update restrict;
 
