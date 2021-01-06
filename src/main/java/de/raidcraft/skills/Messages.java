6db8bd5df3c271169ed13012cd3b9727d1a53175
Silthus <michael@reichenbach.in>
2021-01-06 23:40:02 +0100

    fix: show only 4 skills per page to make place for child skills

--- src/main/java/de/raidcraft/skills/Messages.java
+++ src/main/java/de/raidcraft/skills/Messages.java
@@ -443,7 +443,7 @@ public final class Messages {
 
         Pagination<ConfiguredSkill> pagination = Pagination.builder()
                 .width(Pagination.WIDTH - 6)
-                .resultsPerPage(6)
+                .resultsPerPage(4)
                 .build(header, new Pagination.Renderer.RowRenderer<>() {
                     @Override
                     public @NonNull Collection<Component> renderRow(ConfiguredSkill value, int index) {
