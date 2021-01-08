fbc1c7fe558fd754e900e04cd2a7eb385515db90
Silthus <michael@reichenbach.in>
2021-01-08 17:43:57 +0100

    feat: add option to enable skills only in specific worlds
    Closes #65

--- src/main/java/de/raidcraft/skills/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/skills/listener/PlayerListener.java
@@ -1,10 +1,14 @@
 package de.raidcraft.skills.listener;
 
 import de.raidcraft.skills.SkillManager;
+import de.raidcraft.skills.entities.SkilledPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.List;
 
 public class PlayerListener implements Listener {
 
@@ -24,5 +28,20 @@ public class PlayerListener implements Listener {
     public void onPlayerQuit(PlayerQuitEvent event) {
 
         skillManager.unload(event.getPlayer());
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onChangeWorld(PlayerChangedWorldEvent event) {
+
+        SkilledPlayer.getOrCreate(event.getPlayer())
+                .activeSkills()
+                .forEach(skill -> {
+                    List<String> worlds = skill.configuredSkill().worlds();
+                    if (worlds.size() > 0 && !worlds.contains(event.getPlayer().getWorld().getName().toLowerCase())) {
+                        skill.disable();
+                    } else {
+                        skill.enable();
+                    }
+                });
     }
 }
